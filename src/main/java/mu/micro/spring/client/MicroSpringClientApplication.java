fb624a37123a2c730b6d7912884be31c1b533cf9
舒先 <shuxian@huize.com>
2019-03-28 15:01:11 +0800

    init

---
+++ src/main/java/mu/micro/spring/client/MicroSpringClientApplication.java
@@ -0,0 +1,13 @@
+package mu.micro.spring.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSpringClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroSpringClientApplication.class, args);
+    }
+
+}
